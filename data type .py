4f3318be9be8c912c4a5901f286c8892084ee78c
Havish12345 <havish.somaiahm@gmail.com>
2025-01-25 16:57:36 +0530

    Add files via upload

---
+++ data type .py
@@ -0,0 +1,11 @@
+
+
+number=8.334
+
+print('data type of nymber is',type(number) )
+
+print("after type casting" )
+
+number=int(number)
+print('data type of nymber is',type(number) )
+print(number)
