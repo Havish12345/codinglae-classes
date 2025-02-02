331d6eb48acd047e354c92dc7605d4c73f34023b
Havish12345 <havish.somaiahm@gmail.com>
2025-02-02 08:23:28 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,7 @@
+print("Hi rohan i can help you decide what to wear today.\nenter the temprature in celsius")
+temp=float(input("Enter the temperature:"))
+if temp >= 20:
+       print("you can where light clothes.")
+else:
+       print("yuo should were something warmer.")
+
