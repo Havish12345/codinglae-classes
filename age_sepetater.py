6b145e88215e6841fa3f393284103cb769417bdf
Havish12345 <havish.somaiahm@gmail.com>
2025-02-14 17:13:27 +0530

    Add files via upload

---
+++ age_sepetater.py
@@ -0,0 +1,10 @@
+
+print("Raj please enter the age")
+age = int(input("Enter your age: "))
+if age <=20:
+    if age >=10:
+        print("you can enrol for the class")
+    else:
+      print("you can not enrol for the class")
+else:
+    print("you can not enrol for the class")      
