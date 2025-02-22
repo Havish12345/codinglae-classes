9079a13ba868e4c3b93898a050c708eaa0380fa2
Havish12345 <havish.somaiahm@gmail.com>
2025-02-22 16:41:35 +0530

    Add files via upload

---
+++ nested_loop.py
@@ -0,0 +1,21 @@
+charecter=str(input("Enter a charecter: "))
+letter=str(input("Enter a letter: "))
+i=0
+count=0
+while i<len(charecter):
+    if charecter[i]==letter:
+        count+=1
+    i=i+1
+print(count)
+
+
+
+
+
+
+
+
+
+
+
+
