b5f1ca80444426d34040d188324cb5ba9913d880
Havish12345 <havish.somaiahm@gmail.com>
2025-03-09 09:18:55 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,9 @@
+def print_statement():
+    print('This is a statement')
+    print('This is another statement')
+    print('This is the last statement')
+
+for i in range(5):
+
+
+   print_statement()    
