e746fa4fa2b618cf52bcce4f5c86ccf11f38e76a
Havish12345 <havish.somaiahm@gmail.com>
2025-03-09 09:41:06 +0530

    Add files via upload

---
+++ functions2.py
@@ -0,0 +1,19 @@
+def add(p1,p2):
+    sum_of_numbers=p1+p2
+    return sum_of_numbers
+number1=10
+number2=20
+result=add(number1,number2)
+print(result)
+
+number3=30
+number4=40
+
+
+result1=add(number3,number4)
+print(result1)
+
+
+
+
+
