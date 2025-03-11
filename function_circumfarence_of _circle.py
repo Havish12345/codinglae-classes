a23db601bac62486d9f6fe8678b1c5969cacfbdd
Havish12345 <havish.somaiahm@gmail.com>
2025-03-11 22:54:33 +0530

    Add files via upload

---
+++ function_circumfarence_of _circle.py
@@ -0,0 +1,5 @@
+pie=22/7
+def circumference_of_circle(r):
+    return 2*pie*r
+r=float(input("Enter the radius of the circle: "))
+print("The circumference of the circle is: ",circumference_of_circle(r))
