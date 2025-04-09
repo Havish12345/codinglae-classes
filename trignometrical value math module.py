745fec7d20f495a8b978a5b55f055bb24092f9c5
Havish12345 <havish.somaiahm@gmail.com>
2025-04-09 22:21:47 +0530

    Add files via upload

---
+++ trignometrical value math module.py
@@ -0,0 +1,10 @@
+import math
+angle =int(input("Enter the angle in degrees: "))
+angle= math.radians(angle) # converting degrees to radians
+sin= math.sin(angle)
+cos= math.cos(angle)
+tan= math.tan(angle)
+print("The value of sin is: ",sin)
+print("The value of cos is: ",cos)
+print("The value of tan is: ",tan)
+
